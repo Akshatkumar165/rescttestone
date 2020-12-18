33247a6c84014dd535ba48659ea7076fb2d01948
Akshatkumar <ak3110540@gmail.com>
2020-12-18 10:07:31 +0530

    link added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My awesome react app at learn code online.
+          My awesome react app at learncodeonline.in
         </p>
         
       </header>
